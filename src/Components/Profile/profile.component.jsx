4e1279ee3287f8c44ec1745111f2eacecbe02bca
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-10-28 04:21:33 +0100

    setting button states

--- src/Components/Profile/profile.component.jsx
+++ src/Components/Profile/profile.component.jsx
@@ -1,24 +1,46 @@
+import { useState } from "react";
+
 import "./profile.style.css";
-import profile from '../../assets/img/display-pic.svg';
-import profileHover from "../../assets/img/display-pic-hover.svg";
-import more from '../../assets/img/more-Default.svg'
-import share from '../../assets/img/Share-Default.svg'
+import profile from '../../assets/img/ceejay.webp';
+import more from '../../assets/img/more-Default.svg';
+import share from '../../assets/img/Share-Default.svg';
+import tool from '../../assets/img/Tooltip.svg';
+import clicked from '../../assets/img/State-Focus.svg'
+import cam from '../../assets/img/cam.svg'
+
 
 const Profile = () => {
+  const [isActive, setIsActive] = useState(false)
+  const [showTooltip, setShowTooltip] = useState(false)
+
+
+  const handle = () => {
+    setIsActive(current => !current)
+  }
   return (
     <div className="head">
       <div className="profile">
         <div className="user-info">
-          <div>
-            <img src={profile} alt="" id="profile__img" />
-            <img src={profileHover} alt="" className="bg-hover" />
+          <div className="profile-sec">
+            <div className="img" style={{
+              backgroundImage: `url(${profile})`
+            }} />
+            <div className="overlay">
+              <img src={cam} alt="" className="cam" />
+            </div>
           </div>
           <p className="twitter" id="twitter">Ceejay__1</p>
           <p className="twitter" id="slack" hidden>Cii-jay</p>
         </div>
         <div className="nav-button">
           <img src={more} alt="" className="more-button" />
-          <img src={share} alt="" className="share-button" />
+          <div className="share-share">
+            <img src={tool} alt="tool-tip" className="tool" style={showTooltip ? { visibilty: "visible" } : { visibility: "hidden" }} />
+            <img src={isActive ? clicked : share} alt="" className="share-button" onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)}
+              onClick={handle}
+            />
+
+          </div>
         </div>
 
       </div>
